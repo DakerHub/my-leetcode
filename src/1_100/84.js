8236770b3b1396555d4cb3e3b33ac2a3498c191d
Daker <523246656@qq.com>
2021-02-25 20:33:13 +0800

    [answer] #84

--- src/1_100/84.js
+++ src/1_100/84.js
@@ -11,10 +11,39 @@
 */
 
 /**
+ * 单调递增栈
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function (heights) {};
+var largestRectangleArea = function (heights) {
+  let maxHeight = 0;
+  const stack = [];
+  const getTop = (stack) => {
+    return stack.length ? stack[stack.length - 1] : -1;
+  };
+
+  for (let i = 0; i < heights.length; i++) {
+    const h = heights[i];
+
+    let needToCalc = stack[stack.length - 1];
+    while (h < heights[needToCalc]) {
+      stack.pop();
+      let newTop = getTop(stack);
+      maxHeight = Math.max((i - newTop - 1) * heights[needToCalc], maxHeight);
+      needToCalc = getTop(stack);
+    }
+
+    stack.push(i);
+  }
+
+  while (stack.length !== 0) {
+    const needToCalc = stack.pop();
+    let newTop = getTop(stack);
+    maxHeight = Math.max((heights.length - newTop - 1) * heights[needToCalc], maxHeight);
+  }
+
+  return maxHeight;
+};
 
 module.exports = {
   largestRectangleArea,
--- src/1_100/84.test.js
+++ src/1_100/84.test.js
@@ -6,3 +6,11 @@ const fns = [largestRectangleArea];
 test('[2,1,5,6,2,3] 结果为 10', () => {
   expect(testMultiFn(fns, [2, 1, 5, 6, 2, 3])).toBe(10);
 });
+
+test('[2,4] 结果为 4', () => {
+  expect(testMultiFn(fns, [2, 4])).toBe(4);
+});
+
+test('[4,4,4] 结果为 12', () => {
+  expect(testMultiFn(fns, [4, 4, 4])).toBe(12);
+});
