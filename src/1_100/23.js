af879a1e9a55b86057e59c2209171d716cfeec6f
Daker <523246656@qq.com>
2021-03-28 21:50:49 +0800

    [q] #23

---
+++ src/1_100/23.js
@@ -0,0 +1,20 @@
+// 23. 合并K个升序链表
+
+// 给你一个链表数组，每个链表都已经按升序排列。
+
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {};
+
+module.exports = { mergeKLists };
